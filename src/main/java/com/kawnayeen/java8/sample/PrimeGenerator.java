5eeaeb2e9fe893bb8320015b82e76117c5bbff12
kamarul kawnayeen <kawnayeen.08@gmail.com>
2017-01-23 22:52:46 +0600

    prime number generator development in progress

---
+++ src/main/java/com/kawnayeen/java8/sample/PrimeGenerator.java
@@ -0,0 +1,18 @@
+package com.kawnayeen.java8.sample;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Created by kawnayeen on 1/23/17.
+ */
+public class PrimeGenerator {
+    public List<Integer> generateFirstNPrime(int numberOfPrime) {
+        List<Integer> temp = Stream.iterate(1, e -> e + 1)
+                .filter(PrimeChecker::isPrime)
+                .limit(numberOfPrime)
+                .collect(Collectors.toList());
+        return temp;
+    }
+}
---
+++ src/test/java/com/kawnayeen/java8/sample/PrimeCheckerTest.java
@@ -0,0 +1,34 @@
+package com.kawnayeen.java8.sample;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by kawnayeen on 1/23/17.
+ */
+public class PrimeCheckerTest {
+    @Test
+    void testIsPrime(){
+        assertTrue(PrimeChecker.isPrime(3));
+        assertTrue(PrimeChecker.isPrime(7));
+        assertTrue(PrimeChecker.isPrime(11));
+    }
+
+    @Test
+    void testIsNotPrime(){
+        assertFalse(PrimeChecker.isPrime(4));
+        assertFalse(PrimeChecker.isPrime(49));
+        assertFalse(PrimeChecker.isPrime(15));
+    }
+
+    @Test
+    void testGenerateFirst5PrimeNumber(){
+        PrimeGenerator primeGenerator = new PrimeGenerator();
+        List<Integer> first5Prime = primeGenerator.generateFirstNPrime(5);
+        assertNotNull(first5Prime);
+        assertEquals(5,first5Prime.size());
+    }
+}
