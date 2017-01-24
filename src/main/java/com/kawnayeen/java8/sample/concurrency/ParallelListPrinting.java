ec69a2764f73c6a8f5839249a72c6f67bd4f89cd
kawnayeen <kawnayeen.08@gmail.com>
2017-01-24 16:28:47 +0600

    one more parallel stream example added. anyway, multithread class aren’t test using junit, they have main method to run them.

--- src/main/java/com/kawnayeen/java8/sample/concurrency/ParallelListPrinting.java
+++ src/main/java/com/kawnayeen/java8/sample/concurrency/ParallelListPrinting.java
@@ -1,5 +1,6 @@
 package com.kawnayeen.java8.sample.concurrency;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -8,5 +9,11 @@ import java.util.List;
 public class ParallelListPrinting {
     public void printList(List<Integer> values) {
         values.parallelStream().forEach(System.out::println);
+    }
+
+    public static void main(String[] args){
+        List<Integer> values = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        ParallelListPrinting parallelListPrinting = new ParallelListPrinting();
+        parallelListPrinting.printList(values);
     }
 }
