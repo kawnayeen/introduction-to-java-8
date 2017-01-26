c23b03fb931b3772bd90d031cf3197f48e88d9e9
kawnayeen <kawnayeen.08@gmail.com>
2017-01-26 11:28:12 +0600

    example of Function<T,R> , ToIntFunction<T> , ToDoubleFunction<T> & ToLongFunction<T> added

---
+++ src/main/java/com/kawnayeen/java8/lamda/_3_function_intefaces/_2_Function/_2_ToIntFunction.java
@@ -0,0 +1,25 @@
+package com.kawnayeen.java8.lamda._3_function_intefaces._2_Function;
+
+import java.util.function.ToIntFunction;
+
+/**
+ * Created by kawnayeen on 1/26/17.
+ */
+public class _2_ToIntFunction {
+    public static void main(String[] args){
+        exampleOne();
+        exampleTwo();
+    }
+
+    private static void exampleOne(){
+        ToIntFunction<String> findLength = str->str.length();
+        String myStr = "Kawnayeen";
+        System.out.println("Length of String '"+myStr+"' is : "+findLength.applyAsInt(myStr));
+    }
+
+    private static void exampleTwo(){
+        ToIntFunction<String> findLength = String::length;
+        String myStr = "Kawnayeen";
+        System.out.println("Length of String '"+myStr+"' is : "+findLength.applyAsInt(myStr));
+    }
+}
