c91a79343f776a64c166c0b50d472eee7ad269cb
kawnayeen <kawnayeen.08@gmail.com>
2017-01-26 12:14:33 +0600

    example of IntFunction<R>, DoubleFunction<R> and LongFunction<R> added

---
+++ src/main/java/com/kawnayeen/java8/lamda/_3_function_intefaces/_2_Function/_7_LongFunction.java
@@ -0,0 +1,30 @@
+package com.kawnayeen.java8.lamda._3_function_intefaces._2_Function;
+
+import java.util.function.LongFunction;
+
+/**
+ * Created by kawnayeen on 1/26/17.
+ */
+public class _7_LongFunction {
+    public static void main(String[] args){
+        LongFunction<SocialSecurityNumber> getSSN = SocialSecurityNumber::new;
+
+        SocialSecurityNumber ssn = getSSN.apply(Long.MAX_VALUE);
+        System.out.println(ssn.toString());
+    }
+}
+
+class SocialSecurityNumber{
+    private String ssn;
+
+    public SocialSecurityNumber(Long ssn){
+        this.ssn = ssn.toString();
+    }
+
+    @Override
+    public String toString() {
+        return "SocialSecurityNumber{" +
+                "ssn='" + ssn + '\'' +
+                '}';
+    }
+}
