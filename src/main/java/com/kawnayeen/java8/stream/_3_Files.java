e94e442c1612fd7bfe17d4acbdf4304b78109e23
kawnayeen <kawnayeen.08@gmail.com>
2017-01-28 19:23:43 +0600

    example of getting stream from file added

--- src/main/java/com/kawnayeen/java8/stream/_3_Files.java
+++ src/main/java/com/kawnayeen/java8/stream/_3_Files.java
@@ -2,8 +2,11 @@ package com.kawnayeen.java8.stream;
 
 import java.io.IOException;
 import java.nio.file.FileSystems;
+import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.function.BiPredicate;
 import java.util.stream.Stream;
 
 /**
@@ -11,8 +14,43 @@ import java.util.stream.Stream;
  */
 public class _3_Files {
     public static void main(String[] args) throws IOException {
-        Path path = FileSystems.getDefault().getPath("storage/text.txt");
-        Stream<String> lineStream = Files.lines(path);
+        streamOfLinesFromAFile();
+        streamOfPathFromADirectory();
+        streamOfFilePathFromADirectoryByWalking();
+        streamOfFilePathFromADirectoryByFind();
+    }
+
+    private static void streamOfLinesFromAFile() throws IOException {
+        System.out.println("Printing lines from a File :- ");
+        Path filePath = FileSystems.getDefault().getPath("storage/text.txt");
+        Stream<String> lineStream = Files.lines(filePath);
         lineStream.forEach(System.out::println);
+        System.out.println();
+    }
+
+    private static void streamOfPathFromADirectory() throws IOException {
+        System.out.println("Printing paths from a directory :-");
+        Path directoryPath = FileSystems.getDefault().getPath("storage");
+        Stream<Path> pathStream = Files.list(directoryPath);
+        pathStream.forEach(System.out::println);
+        System.out.println();
+    }
+
+    private static void streamOfFilePathFromADirectoryByWalking() throws IOException {
+        System.out.println("Printing paths from a directory recursively:-");
+        Path directoryPath = FileSystems.getDefault().getPath("storage");
+        Stream<Path> pathStream = Files.walk(directoryPath, FileVisitOption.FOLLOW_LINKS);
+        pathStream.forEach(System.out::println);
+        System.out.println();
+    }
+
+    private static void streamOfFilePathFromADirectoryByFind() throws IOException {
+        System.out.println("Printing paths from a directory that match given condition:- ");
+        BiPredicate<Path, BasicFileAttributes> expectedPath = (path, attrs) ->
+                attrs.isRegularFile() && path.toString().endsWith(".txt");
+        Path directoryPath = FileSystems.getDefault().getPath("storage");
+        Stream<Path> foundedPath = Files.find(directoryPath, 3, expectedPath);
+        foundedPath.forEach(System.out::println);
+        System.out.println();
     }
 }
