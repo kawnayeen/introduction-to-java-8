fa662946711b58a0d60eb5fc50aa69cfa8889ea5
kamarul kawnayeen <kawnayeen.08@gmail.com>
2017-06-17 21:43:49 +0600

    example of modifying existing file added

--- src/main/java/com/kawnayeen/java8/stream/_1_stream_srouce/_3_Files.java
+++ src/main/java/com/kawnayeen/java8/stream/_1_stream_srouce/_3_Files.java
@@ -1,11 +1,14 @@
 package com.kawnayeen.java8.stream._1_stream_srouce;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.FileSystems;
 import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import java.util.function.BiPredicate;
 import java.util.stream.Stream;
 
@@ -14,10 +17,11 @@ import java.util.stream.Stream;
  */
 public class _3_Files {
     public static void main(String[] args) throws IOException {
-        streamOfLinesFromAFile();
-        streamOfPathFromADirectory();
-        streamOfFilePathFromADirectoryByWalking();
-        streamOfFilePathFromADirectoryByFind();
+        //streamOfLinesFromAFile();
+        //streamOfPathFromADirectory();
+        //streamOfFilePathFromADirectoryByWalking();
+        //streamOfFilePathFromADirectoryByFind();
+        addSurahNumberAndAyatNumberAtEachAyatFile();
     }
 
     private static void streamOfLinesFromAFile() throws IOException {
@@ -52,5 +56,35 @@ public class _3_Files {
         Stream<Path> foundedPath = Files.find(directoryPath, 3, expectedPath);
         foundedPath.forEach(System.out::println);
         System.out.println();
+    }
+
+    private static void addSurahNumberAndAyatNumberAtEachAyatFile() throws IOException {
+        System.out.println("Printing paths from a directory that match given condition:- ");
+        BiPredicate<Path, BasicFileAttributes> expectedPath = (path, attrs) ->
+                attrs.isRegularFile() && path.toString().endsWith(".json");
+        Path directoryPath = FileSystems.getDefault().getPath("001");
+        Stream<Path> foundedPath = Files.find(directoryPath, 3, expectedPath);
+        foundedPath.forEach(_3_Files::addSurahAndAyatName);
+        System.out.println();
+    }
+
+    private static void addSurahAndAyatName(Path path) {
+        try {
+            Scanner scanner = new Scanner(new FileInputStream(path.toFile()));
+            List<String> exitingContent = new ArrayList<>();
+            while (scanner.hasNextLine()) {
+                exitingContent.add(scanner.nextLine());
+            }
+            String fileName = path.getFileName().toString();
+            String ayatNumber = fileName.substring(3, 6);
+            String surahNumber = fileName.substring(0, 3);
+            exitingContent.add(2, "\"surahNumber\":\"" + surahNumber + "\",");
+            exitingContent.add(3, "\"ayatNumber\":\"" + ayatNumber + "\",");
+            PrintWriter printWriter = new PrintWriter(new FileOutputStream(path.toFile(), false));
+            exitingContent.forEach(printWriter::println);
+            printWriter.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
